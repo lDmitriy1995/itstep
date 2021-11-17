57886114e7ca89a0915e19781f013e4eaace075c
lDmitriy1995 <94545009+lDmitriy1995@users.noreply.github.com>
2021-11-17 21:12:23 +0600

    Create itsteploc.cpp

---
+++ itsteploc.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Magic" << std::endl;
+	std::cout << "Дамблдор" << std::endl;
+	std::cout << "Потный Гарри" << std::endl;
+}
