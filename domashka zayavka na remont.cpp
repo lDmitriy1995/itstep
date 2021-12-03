5ca54cf521237129d22679a879032836ec9690ae
lDmitriy1995 <94545009+lDmitriy1995@users.noreply.github.com>
2021-12-03 14:49:17 +0600

    Create domashka zayavka na remont.cpp

---
+++ domashka zayavka na remont.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "windows.h"
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string name{};
+    std::string telefon{};
+    std::string model{};
+   
+    std::cout << " Добро пожаловать в автосервис <Мастерская синдиката> "<< std::endl;
+    std::cout << " Заполните заявку на ремонт автомобиля" << std::endl;
+    std::cout << " Укажите свое имя, номер телефона и марку автомобиля " << std::endl << std::endl;
+
+    std::cout << ">> Введите свое имя " << std::endl;
+    std::cin >> name;
+    
+    std::cout << ">> Введите номер телефона " << std::endl;
+    std::cin >> telefon;
+
+    std::cout << ">>Введите модель авто " << std::endl;
+    std::cin >> model;
+    
+    std::cout << std::endl << std::endl;
+    std::cout << " Ваши данные: " << std::endl;
+    std::cout << " Имя: " <<name << std::endl;
+    std::cout << " Номер телефона: " << telefon << std::endl;
+    std::cout << " Модель авто: " << model << std::endl << std::endl;
+    
+    std::cout << " Спасибо за обращение в сервис <Мастерская синдиката> "<< std::endl;
+    std::cout << " Наш оператор с вами свяжется через 2-3 дня "<< std::endl;
+}
