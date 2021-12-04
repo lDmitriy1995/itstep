16c0f532e4201284b5d613e35dbf421772d88a6c
lDmitriy1995 <94545009+lDmitriy1995@users.noreply.github.com>
2021-12-04 11:57:56 +0600

    Update domashka zayavka na remont.cpp

--- domashka zayavka na remont.cpp
+++ domashka zayavka na remont.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "windows.h"
+#include <string>
 
 
 int main()
@@ -9,26 +10,26 @@ int main()
     std::string name{};
     std::string telefon{};
     std::string model{};
-   
-    std::cout << " Добро пожаловать в автосервис <Мастерская синдиката> "<< std::endl;
+
+    std::cout << " Добро пожаловать в автосервис <Мастерская синдиката> " << std::endl;
     std::cout << " Заполните заявку на ремонт автомобиля" << std::endl;
     std::cout << " Укажите свое имя, номер телефона и марку автомобиля " << std::endl << std::endl;
 
     std::cout << ">> Введите свое имя " << std::endl;
-    std::cin >> name;
-    
+    std::getline(std::cin, name);
+
     std::cout << ">> Введите номер телефона " << std::endl;
-    std::cin >> telefon;
+    std::getline(std::cin, telefon);
 
     std::cout << ">>Введите модель авто " << std::endl;
-    std::cin >> model;
-    
+    std::getline(std::cin, model);
+
     std::cout << std::endl << std::endl;
     std::cout << " Ваши данные: " << std::endl;
-    std::cout << " Имя: " <<name << std::endl;
+    std::cout << " Имя: " << name << std::endl;
     std::cout << " Номер телефона: " << telefon << std::endl;
     std::cout << " Модель авто: " << model << std::endl << std::endl;
-    
-    std::cout << " Спасибо за обращение в сервис <Мастерская синдиката> "<< std::endl;
-    std::cout << " Наш оператор с вами свяжется через 2-3 дня "<< std::endl;
+
+    std::cout << " Спасибо за обращение в сервис <Мастерская синдиката> " << std::endl;
+    std::cout << " Наш оператор с вами свяжется через 2-3 дня " << std::endl;
 }
