ddbaf1cbc4da28d06ac9392934079ae9c62046cd
lDmitriy1995 <94545009+lDmitriy1995@users.noreply.github.com>
2022-03-23 23:51:57 +0600

    Create 24.02.2022Dz.cpp

---
+++ 24.02.2022Dz.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	/*int min{ 10 };                                                  //===========Задание №1
+	int max{ 88 };
+	int summ{ 0 };
+	while (min < max || min == max)
+	{
+		summ += min;
+		min++;
+	}
+	cout << summ << endl;
+	cout << endl;*/
+
+
+	//int min{ 5 };                                                    //===========Задание №2
+	//int max{ 13 };
+	//int summ{ 1 };
+	//while (min < max || min == max)
+	//{
+	//	summ *= min;
+	//	min++;
+	//}
+	//cout << summ << endl;
+	//cout << endl;
+
+	/*int min{ 1 };                                                   //===========Задание №3
+	int max{ 112 };
+	int summ{ 0 };
+	while (min < max || min == max)
+	{
+		summ += min;
+		min += 3;
+	}
+	cout << summ << endl;
+	cout << endl;*/
+
+	
+	
+   /*double sum = 0.0;                                              //===========Задание №4
+
+   for (int i = 2; i <= 9; ++i)
+   sum += ((double)i) / (double(i + 1));
+
+   cout << "Result: " << sum << endl;
+ 
+   return 0;*/
+	
+	/*int min{ 1 };                                                  //===========Задание №5 
+	int max{ 1000 };
+	int summ = 0;
+	while (min < 101)
+	{
+		if (min == 1)
+		{
+			summ = min	;
+			min++;
+		}
+		else
+		{
+			summ += min;
+			min++;
+			cout << summ << endl;
+		}
+	}*/
+
+
+	/*int n{ 15 };                                                      //===========Задание №6
+	int min = 1;
+	int summ = 0;
+	int k{ 0 };
+	while (min < (n + 1))
+	{
+		k = min * min;
+		summ += k;
+		min++;
+	}
+	cout << endl;
+	cout << summ << endl;
+	*/
+
+	/*double sum = 0;                                                  //===========Задание №7
+	int n = 10;
+	for (int i = 1; i <= n; i++)
+		sum += (1. / i);
+	cout << sum << endl;
+
+
+}*/
