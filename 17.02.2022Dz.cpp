7eb2989d056eb93f851abf69ca9005151709c295
lDmitriy1995 <94545009+lDmitriy1995@users.noreply.github.com>
2022-03-23 19:42:32 +0600

    Create 17.02.2022Dz.cpp

---
+++ 17.02.2022Dz.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+//int func(int m2, int m1, int m0)                                                 //===========Задание № 7 Функция
+//{
+//	int m{ 0 };
+//	m2 = m2 % 1000 / 100 * 100;
+//	m1 = m1 % 100 / 10 * 10;
+//	m0 = m0 % 10;
+//	m = m2 + m1 + m0;
+//	return m;
+//}
+
+//int func2(int x, int y)                                                          //===========Задание № 8 Функция
+//{
+//	int w{ 0 };
+//	if (y == 0 && x != 0)
+//	{
+//		w = 12 / x;
+//	}
+//	else if (x == 0 && y != 0)
+//	{
+//		w = 12 / y;
+//	}
+//	else if (x != 0 && y != 0)
+//	{
+//		w = 144 / (x * y);
+//	}
+//	return w;
+//}
+//
+//
+//int main()
+//{
+//	SetConsoleCP(1251);
+//	SetConsoleOutputCP(1251);
+
+	/*int a;                                                                             //===========Задание № 1
+	int b;
+	int c;
+	cout << "Введите число" << endl;
+	cin >> a;
+
+	b = a * a;
+	c = b * b;
+	cout << a << " в четвертой степени = " << c;*/
+
+
+	/*int a;                                                                            
+	int b;
+	int c;
+	int d;
+	cout << "Введите число" << endl;
+	cin >> a;
+
+	b = a * a;
+	c = b * b;
+	d = b * c;
+	cout << a << " в шестой степени = " << d; */
+
+	/*int a;                                                                            
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+
+	cout << "Введите число" << endl;
+	cin >> a;
+
+	b = a * a;
+	c = b * a;
+	d = c * c;
+	e = d * d;
+	f = e * c;
+
+	cout << a << " в пятнадцатой степени = " << f;*/
+
+	/*int x{ 6 };                                                                       //===========Задание № 2
+	int y{ 8 };
+	int z{ 2 };
+	int s{ 0 };
+	int R ={ 0 };
+
+	s = x * y;
+	z *= z;
+	R = s / z;
+	std::cout << "Количество квадратов: " << R << std::endl;*/
+
+	/*int a{3};                                                                         //===========Задание № 3                    
+	int b{6};
+	int c{9};
+
+	if (a < b && b < c)
+	{
+
+		cout << "Равенство a<b<c верное!"<< endl;
+	}
+
+	else
+	{
+		cout << "Равенство a<b<c не верное!" << endl;;
+	}
+	if (b > a && a > c)
+	{
+		cout << "Равенство b>a>c верное!" << endl;
+	}
+	else
+	{
+		 cout << "Равенство b>a>c не верное!" << endl;
+	}*/
+
+
+	/*int a{5};                                                                           //===========Задание № 4
+	int b{8};
+	int c{5};
+
+	if (a == b || b == c || a == c)
+	{
+
+		cout << "Есть равные числа ";
+	}
+
+	else
+	{
+		cout << "Равных чисел  нет ";
+	}*/
+
+
+	/*for (int i = 0; i < 4; i++)                                                            //===========Задание № 5
+	{
+		for (int j = 0; j < 7; j++)
+		{
+			cout << "*";
+		}
+
+		cout << endl;
+	}*/
+
+	/*int a{ 1 };                                                                            //===========Задание № 6
+	int b{ 112 };
+	int c{ 3 };
+	int d{ 1 };
+
+	while (a < b)
+	{
+
+		a += c;
+		d += a;
+	}
+	cout << d;*/
+
+	
+
+
+
+	/*int m2{ 358 };                                                                        //===========Задание № 7
+	int m1{ 115 };
+	int m0{ 76 };
+
+	cout << func(m2, m1, m0);*/
+
+ /*      
+	int x{ 6 };                                                                            //===========Задание № 8
+	int y{ 9 };
+   
+    cout << func2(x,y);
+
+
+}
+*/
