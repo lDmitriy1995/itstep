0fd03ae656cef054639855d50d7ab42377738e47
lDmitriy1995 <94545009+lDmitriy1995@users.noreply.github.com>
2022-03-25 00:04:55 +0600

    Create 17.03.2022Dz.cpp

---
+++ 17.03.2022Dz.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <Windows.h>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+//void rectangle(int x, int y)                                           //===========Функция № 1
+//{
+//    for (int i = 0; i < x; i++)
+//    {
+//        for (int j = 0; j < y; j++)
+//        {
+//            cout << " * ";
+//        }
+//        cout << endl;
+//    }
+//    
+//}
+
+//void circle(int R)                                                      
+//void rectangle(int x, int y)                                           //===========Функция № 2
+//{
+//    int n;
+//    /*size_t R;*/
+//    size_t N;
+//    double di;
+//    cout << "Enter R: \n";
+//    cin >> R;
+//    N = 2 * R;
+//    di = 1.0 / R;
+//    for (int i = int(R); i >= -int(R); --i) {
+//        n = static_cast<int>(cos(asin(di * abs(int(i)))) * R);
+//        for (int j = 0; j <= R - n; ++j) cout << " ";
+//        cout << "*";
+//        for (int j = 0; j < 2 * n; ++j) cout << " ";
+//        cout << "*\n";
+//    }
+//}
+
+
+//void triangle(int x)                                                         
+//void rectangle(int x, int y)                                           //===========Функция № 3
+//{
+//    for (size_t i = 0; i < x; ++i)
+//    {
+//        for (size_t j = 0; j <= i; ++j)
+//            cout << "*";
+//            cout << endl;
+//    }
+//}
+
+
+int main() {
+    system("color 0B");
+    setlocale(0, "ru");
+ 
+    /*int x{ 6 };                                                       //===========Задание №1
+    int y{ 8 };
+    rectangle(x, y);*/
+
+ 
+    /*int R{0};                                                         //===========Задание №2
+    circle(R);*/
+
+    //int x{ 8 };                                                       //===========Задание №3
+    //triangle(x);
+
+}
